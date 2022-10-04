0e0df2f5c47a1301c82e7c5e66305d6ae3f21c30
Idris Mahamdi <73561935+idrismahamdi@users.noreply.github.com>
2022-10-04 20:00:37 +0100

    removing extra 's'

--- Database Creation and Inserts/Functions.sql
+++ Database Creation and Inserts/Functions.sql
@@ -1,5 +1,5 @@
 DELIMITER //
-CREATE FUNCTION login(uName VARCHAR(255), uPasssword VARCHAR(255)) RETURNS BIT
+CREATE FUNCTION login(uName VARCHAR(255), uPassword VARCHAR(255)) RETURNS BIT
 BEGIN
 	if EXISTS(select * from user where uName = user.user_name and uPassword = user.user_password)
 		THEN
