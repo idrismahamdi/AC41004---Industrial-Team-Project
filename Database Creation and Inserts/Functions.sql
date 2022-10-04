c25e5be060826e70529af2f7862c4610ed278b73
Idris Mahamdi <73561935+idrismahamdi@users.noreply.github.com>
2022-10-04 19:59:07 +0100

    checking for username instead

--- Database Creation and Inserts/Functions.sql
+++ Database Creation and Inserts/Functions.sql
@@ -1,7 +1,7 @@
 DELIMITER //
 CREATE FUNCTION login(uName VARCHAR(255), uPasssword VARCHAR(255)) RETURNS BIT
 BEGIN
-	if EXISTS(select * from user where uName = user.user_id and uPassword = user.user_password)
+	if EXISTS(select * from user where uName = user.user_name and uPassword = user.user_password)
 		THEN
 			return 1;
 	END if;
