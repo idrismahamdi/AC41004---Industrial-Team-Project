00a1889431f74939f8293e732c3d566a3de2d86c
Idris Mahamdi <73561935+idrismahamdi@users.noreply.github.com>
2022-10-06 18:12:43 +0100

    Update Functions.sql

--- Database Creation and Inserts/Functions.sql
+++ Database Creation and Inserts/Functions.sql
@@ -1,11 +1,11 @@
 
 -- Login verifcation for users--
 DELIMITER //
-CREATE FUNCTION login(uName VARCHAR(255), uPassword VARCHAR(255)) RETURNS BIT
+CREATE FUNCTION login(uName VARCHAR(255), uPassword VARCHAR(255)) RETURNS INT
 BEGIN
 	if EXISTS(select * from user where uName = user.user_name and uPassword = user.user_password)
 		THEN
-			return 1;
+			return user.user_id;
 
 	ELSE 
 		        return 0;
