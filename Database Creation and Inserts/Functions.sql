ecfb2578b7a4af824cbf2d72a4a823c4731c0f7a
Stiffcliff <55393295+Stiffcliff@users.noreply.github.com>
2022-10-07 16:09:27 +0100

    Added non compliant for rule function

--- Database Creation and Inserts/Functions.sql
+++ Database Creation and Inserts/Functions.sql
@@ -52,3 +52,19 @@ BEGIN
 	WHERE rule.rule_id = ruleID AND customer.customer_id = cID AND non_compliance.resource_id = resource.resource_id;
 END //
 DELIMITER ;
+
+-- gets non compliant resources for a rule -- 
+
+DELIMITER //
+CREATE PROCEDURE get_non_compliant_resource_for_rules(ruleID int, cID int)
+BEGIN
+	SELECT resource.resource_name
+	FROM resource
+	LEFT JOIN resource_type ON resource.resource_type_id = resource_type.resource_type_id
+	LEFT JOIN rule ON resource_type.resource_type_id = rule.resource_type_id
+    LEFT JOIN account ON account.account_id = resource.account_id
+    LEFT JOIN customer ON customer.customer_id = account.account_id
+    LEFT JOIN non_compliance ON non_compliance.resource_id = resource.resource_id
+	WHERE rule.rule_id = ruleID AND customer.customer_id = cID AND non_compliance.resource_id = resource.resource_id;
+END //
+DELIMITER ;
