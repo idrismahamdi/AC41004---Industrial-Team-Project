7ad43499078544edfe7937a60eed824e7b777879
Stiffcliff <55393295+Stiffcliff@users.noreply.github.com>
2022-10-07 16:10:46 +0100

    Updated to most recent version
    added latest functions and updated statements

--- Database Creation and Inserts/Database and Table Creation.sql
+++ Database Creation and Inserts/Database and Table Creation.sql
@@ -428,7 +428,7 @@ SELECT account.account_ref FROM account, customer, user WHERE account.customer_i
 END //
 DELIMITER ;
 
--- get resoureces based off rule id and customer id--
+-- get resources based off rule id and customer id--
 
 DELIMITER //
 CREATE PROCEDURE get_resource_for_rules(ruleID int, cID int)
@@ -442,3 +442,20 @@ BEGIN
 	WHERE rule.rule_id = ruleID AND customer.customer_id = cID;
 END //
 DELIMITER ;
+
+-- gets non compliant resources for a rule -- 
+
+DELIMITER //
+CREATE PROCEDURE get_non_compliant_resource_for_rules(ruleID int, cID int)
+BEGIN
+	SELECT resource.resource_name
+	FROM resource
+	LEFT JOIN resource_type ON resource.resource_type_id = resource_type.resource_type_id
+	LEFT JOIN rule ON resource_type.resource_type_id = rule.resource_type_id
+    LEFT JOIN account ON account.account_id = resource.account_id
+    LEFT JOIN customer ON customer.customer_id = account.account_id
+    LEFT JOIN non_compliance ON non_compliance.resource_id = resource.resource_id
+	WHERE rule.rule_id = ruleID AND customer.customer_id = cID AND non_compliance.resource_id = resource.resource_id;
+END //
+DELIMITER ;
+
