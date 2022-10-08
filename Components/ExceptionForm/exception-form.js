23ce5832deaa6b2f3564604989157e55da25a1fb
Arcfej <mayer.miklos91@gmail.com>
2022-10-08 02:52:59 +0100

    Make exception-form.js work for multiple exception forms on one page.

--- Components/ExceptionForm/exception-form.js
+++ Components/ExceptionForm/exception-form.js
@@ -1,20 +1,23 @@
 import * as bootstrap from 'bootstrap'
 
-const dateSelection = document.getElementsByName('reviewDate');
-const customDateElement = document.getElementById('customDateCollapse');
-const customDateInput = document.getElementById('customDateInput');
-const customDateCollapse = new bootstrap.Collapse(customDateElement, {'toggle': false});
+const exceptionForms = document.getElementsByClassName('exception-form');
+for (let exceptionForm of exceptionForms) {
+    const dateSelection = exceptionForm.getElementsByClassName('btn-check');
+    const customDateElement = exceptionForm.getElementsByClassName('custom-date-collapse')[0];
+    const customDateInput = exceptionForm.getElementsByClassName('custom-date-input')[0];
+    const customDateCollapse = new bootstrap.Collapse(customDateElement, {'toggle': false});
 
-for (let dateSelectionElement of dateSelection) {
-    dateSelectionElement.addEventListener('click', (event) => {
-        if (event.target.value === 'custom') {
-            customDateCollapse.show();
-            customDateInput.setAttribute('required', '');
-        } else {
-            customDateCollapse.hide();
-            customDateInput.removeAttribute('required');
-        }
-    });
+    for (let dateSelectionElement of dateSelection) {
+        dateSelectionElement.addEventListener('click', (event) => {
+            if (event.target.value === 'custom') {
+                customDateCollapse.show();
+                customDateInput.setAttribute('required', '');
+            } else {
+                customDateCollapse.hide();
+                customDateInput.removeAttribute('required');
+            }
+        });
+    }
+
+    exceptionForm.getElementsByClassName('custom-date-check')[0].click();
 }
-
-document.getElementById('custom').click();
