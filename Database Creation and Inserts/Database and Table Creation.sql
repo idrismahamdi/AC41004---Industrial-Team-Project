6d1930c77f171185b0fd64bae1b7d6d901a9c450
Idris Mahamdi <73561935+idrismahamdi@users.noreply.github.com>
2022-10-08 23:11:47 +0100

    Update Database and Table Creation.sql

--- Database Creation and Inserts/Database and Table Creation.sql
+++ Database Creation and Inserts/Database and Table Creation.sql
@@ -481,7 +481,7 @@ DELIMITER ;
 DELIMITER //
 CREATE PROCEDURE get_exceptions_for_resource_for_a_rule(ruleID int, cID int)
 BEGIN
-	SELECT resource.resource_name
+	SELECT resource.resource_name, resource.resource_id
 	FROM resource
 	LEFT JOIN resource_type ON resource.resource_type_id = resource_type.resource_type_id
 	LEFT JOIN rule ON resource_type.resource_type_id = rule.resource_type_id
