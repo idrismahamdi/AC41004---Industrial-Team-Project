cbba3a22277698e0ae6588e25765c5a6e8fd6e96
Arcfej <mayer.miklos91@gmail.com>
2022-10-09 11:45:04 +0100

    Remove pasted code from the wrong place

--- Components/Page/page.php
+++ Components/Page/page.php
@@ -39,59 +39,5 @@
       }
     }
   </script>
-  <div>
-    <h1>Detailed Compliance Report for Rule X</h1>
-  </div>
-
-  <br>
-  <br>
-  <div class = "container">
-      <strong>Resource 1: Resource is X </strong>
-  <form class="exception-form">
-    <fieldset class='row mb-3 align-items-center'>
-      <legend class='col-2 col-form-label'>Review in:</legend>
-      <div class="col col-auto">
-        <div class='btn-group' role='group' aria-label='Review date radio toggle button group'>
-          <input type='radio' class='btn-check' name='reviewDate' id='oneMonth' value='oneMonth' autocomplete='off' required>
-          <label class='btn btn-outline-primary m-0' for='oneMonth'>1 Month</label>
-
-          <input type='radio' class='btn-check' name='reviewDate' id='threeMonths' value='threeMonths' autocomplete='off' required>
-          <label class='btn btn-outline-primary m-0' for='threeMonths'>3 Months</label>
-
-          <input type='radio' class='btn-check' name='reviewDate' id='sixMonths' value='sixMonths' autocomplete='off' required>
-          <label class='btn btn-outline-primary m-0' for='sixMonths'>6 Months</label>
-
-          <input type='radio' class="btn-check" name='reviewDate' id='oneYear' value='oneYear' autocomplete='off' required>
-          <label class='btn btn-outline-primary m-0' for='oneYear'>1 Year</label>
-
-          <input class="btn-check custom-date-check" name='reviewDate' id="custom" value='custom' type='radio' autocomplete='off' required>
-          <label class='btn btn-outline-primary m-0' for='custom'>Custom</label>
-        </div>
-      </div>
-
-      <div class="col-auto">
-        <div class='collapse collapse-horizontal custom-date-collapse' id="customDateCollapse">
-          <input type='date' placeholder="Choose a date" class='form-control custom-date-input' id='customDateInput' name='customDate' required>
-        </div>
-      </div>
-    </fieldset>
-
-    <div class='row mb-3 justify-content-start'>
-      <div class='col-2'>
-        <label for='justification' class='form-label'>Justification:</label>
-      </div>
-      <div class='col'>
-        <textarea class='form-control' id='justification' rows='4' required></textarea>
-      </div>
-    </div>
-
-    <div class="row mb-3 justify-content-start">
-      <div class="col offset-2">
-        <button class="btn btn-primary col" type="submit">Create exception</button>
-      </div>
-    </div>
-  </form>
-</div>
-</div>
 </body>
 </html>
