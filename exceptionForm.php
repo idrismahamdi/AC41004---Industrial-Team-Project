8384e95463c10307e38256b9916a48fd95458ee5
Idris Mahamdi <73561935+idrismahamdi@users.noreply.github.com>
2022-10-11 22:08:43 +0100

    Update exceptionForm.php

--- exceptionForm.php
+++ exceptionForm.php
@@ -49,7 +49,50 @@ if (isset($_POST['logout'])) {
     <br>
   <div class="main">
     <div class="card">
-  <?php include "Components/ExceptionForm/exception-form.html"; ?>
+  <form class="exception-form">
+  <fieldset class='row mb-3 align-items-center'>
+    <legend class='col-2 col-form-label'><strong><u>Review in:</u></strong></legend>
+    <div class="col col-auto">
+      <div class='btn-group' role='group' aria-label='Review date radio toggle button group'>
+        <input type='radio' class='btn-check' name='reviewDate' id='oneMonth' value='oneMonth' autocomplete='off' required>
+        <label class='btn btn-outline-primary m-0' for='oneMonth'>1 Month</label>
+
+        <input type='radio' class='btn-check' name='reviewDate' id='threeMonths' value='threeMonths' autocomplete='off' required>
+        <label class='btn btn-outline-primary m-0' for='threeMonths'>3 Months</label>
+
+        <input type='radio' class='btn-check' name='reviewDate' id='sixMonths' value='sixMonths' autocomplete='off' required>
+        <label class='btn btn-outline-primary m-0' for='sixMonths'>6 Months</label>
+
+        <input type='radio' class="btn-check" name='reviewDate' id='oneYear' value='oneYear' autocomplete='off' required>
+        <label class='btn btn-outline-primary m-0' for='oneYear'>1 Year</label>
+
+        <input class="btn-check custom-date-check" name='reviewDate' id="custom" value='custom' type='radio' autocomplete='off' required>
+        <label class='btn btn-outline-primary m-0' for='custom'>Custom</label>
+      </div>
+    </div>
+
+    <div class="col-auto">
+      <div class='collapse collapse-horizontal custom-date-collapse' id="customDateCollapse">
+        <input type='date' placeholder="Choose a date" class='form-control custom-date-input' id='customDateInput' name='customDate' required>
+      </div>
+    </div>
+  </fieldset>
+
+  <div class='row mb-3 justify-content-start'>
+    <div class='col-2'>
+      <label for='justification' class='form-label'><strong><u>Justification:</strong></u></label>
+    </div>
+    <div class='col'>
+      <textarea class='form-control' id='justification' rows='4' required></textarea>
+    </div>
+  </div>
+
+  <div class="row mb-3 justify-content-start">
+    <div class="col offset-2">
+      <button class="btn btn-primary col" type="submit">Create exception</button>
+    </div>
+  </div>
+</form>
 </div>
 </div>
 <script async src='https://cdn.jsdelivr.net/npm/es-module-shims@1/dist/es-module-shims.min.js' crossorigin='anonymous'></script>
