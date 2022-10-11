54c9d78490df9b4aba80ebd067178df3e8dd84c8
Idris Mahamdi <73561935+idrismahamdi@users.noreply.github.com>
2022-10-11 21:54:10 +0100

    bug fix

--- view.php
+++ view.php
@@ -1,6 +1,18 @@
 <?php
 require "connection.php";
 session_start();
+
+if (isset($_POST['view'])) {
+    $_SESSION['view'] = $_POST['view'];
+    header('Location: view.php');
+    die();
+}
+
+if (isset($_POST['create'])) {
+    header('Location: exceptionForm.php');
+    die();
+}
+
 if ($_SESSION['loggedin'] == false) {
     header('Location: login.php');
     die();
@@ -9,24 +21,11 @@ if (isset($_POST['logout'])) {
     session_destroy();
     header('Location: login.php');
     die();
-}
-
-$query = ('CALL get_exception_for_resource(:rID,1)');
-if (!empty($_SESSION['view'])) {
-
-
-    $stmt = $mysql->prepare($query);
-    $stmt->bindValue(':rID', $_SESSION['view']);
-    $stmt->execute();
-    $result = $stmt->fetchAll();
-}
-
-?>
+} ?>
 <!doctype html>
 <html>
 
 <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -37,7 +36,7 @@ if (!empty($_SESSION['view'])) {
 
 <body>
     <header>
-        <nav class="navbar">
+        <nav class="navbar border">
             <div class="container">
                 <a class="navbar-brand" href="#">
                     <img src="logo.png" height="50px" alt="brightsolid">
@@ -48,85 +47,170 @@ if (!empty($_SESSION['view'])) {
             </div>
         </nav>
     </header>
-
     <br>
     <nav aria-label="breadcrumb" class="main">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="dashboard.php">Summary Dashboard</a></li>
-            <li class="breadcrumb-item"><a href="report.php">Detailed Report</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Exception History</li>
+            <li class="breadcrumb-item active" aria-current="page">Detailed Report</li>
         </ol>
-    </nav>    
-
+    </nav>
     <br>
-    <!--insert name of resource here-->
-    <h1>EXCEPTION HISTORY</h1>
-    <h2>of Resource X</h2>
+    <h1>DETAILED REPORT<h1>
 
 
-    <br>
-    <div class="main">
-    <div class="card">
-    <table class="container text-center overflow-scroll">
-        <?php
+            <?php
 
-        $count = 0;
+            $query = ('CALL get_rule_details(:rID)');
+            $stmt = $mysql->prepare($query);
+            $stmt->bindValue(':rID', $_SESSION['rule']);
+            $stmt->execute();
+            $result = $stmt->fetch();
+            echo '<h2>Created for Rule ', $result[0];
+            echo '</h2>';
+            echo '<p>', $result[1];
+            echo '</p>';
+            ?>
+            <br>
+            <div class="main">
 
-        foreach ($result as $row) {
+                <?php
+                echo '<h1>NON-COMPLIANT RESOURCES</h1>';
 
-            if ($count == 0) {
-                echo '<tr>
-            <th>Rule</th>
-            <th>Last Updated By</th>
-            <th>Justification</th>
-            <th>Review Date</th>
-            <th>Last Updated</th>
-        </tr>';
-            }
+                echo '<div class="card">';
+                $query = ('CALL get_non_compliant_resource_for_rules(:rID, :cID)');
+                $stmt = $mysql->prepare($query);
+                $stmt->bindValue(':rID', $_SESSION['rule']);
+                $stmt->bindValue(':cID', 1);
+                $stmt->execute();
+                $result = $stmt->fetchAll();
+                $myArr = array();
+                $reviewDate = array();
+                $countNonCompliant = 0;
 
-            echo '
-        <tr>
-            <td>
-                ', $row[0], '
-            </td>
-            <td>
-            ', $row[1], '
-            </td>
-            <td>
-            ', $row[2], '
-            </td>
-            <td>
-            ', $row[3], '
-            </td>
-            <td>
-            ', $row[4], '
-            </td>
-            <td>
-                <button name="report" value=', $row[0], ' class="btn btn-info">Update</button>  
-            </td>
-            <td>
-                <button name="report" value=', $row[0], ' class="btn btn-info">Suspend</button>  
-            </td>
-   
-        </tr>
-        ';
-            $count += 1;
-        }
+                foreach ($result as $row) {
 
-        if ($count == 0) {
-            echo '<p style="text-align:center">There is no exception history for this resource.</p>';
-        }
+                    $query = ('CALL get_exception_for_resource(:resourceID,:cID, :rID)');
+                    $stmt = $mysql->prepare($query);
+                    $stmt->bindValue(':resourceID', $row[1]);
+                    $stmt->bindValue(':rID', $_SESSION['rule']);
+                    $stmt->bindValue(':cID', 1);
+                    $stmt->execute();
+                    $test = $stmt->fetch();
+                    if (empty($test)) {
 
-        ?>
-    </table>
-    </div>
-    </div>
-    </div>
-    <br>
-    <footer>
-      <p>Visit our website:<br>
-      <a class="footer-link" href="https://www.brightsolid.com/">BrightSolid</a></p>
-    </footer>
+                        if ($countNonCompliant == 0) {
+                            echo '
+                <table class="table table-borderless">
+                <thead class="thead-dark">
+                    <tr>
+                    <th scope="col">Resource Name</th>
+                    <th scope="col">Exceptions</th>
+                    <th scope="col">Create an Exception</th>
+                    </tr>
+                </thead>';
+                        }
+                        echo '<tr>';
+                        echo '<td>', $row[0], '</td>';
+                        echo ' <td><form action="" method="post"><button name="view" value=', $row[0], ' class="btn btn-info">View</button></form></td>';
+                        echo '<td><form action="" method="post"><button name="create" value=', $row[0], ' class="btn btn-info">Create</button> </form></td>';
+                        echo '</tr>';
+                        $countNonCompliant += 1;
+                    } else {
+                        $myArr[] = $row[0];
+                        $reviewDate[] = $test[1];
+                        $lastUpdated[] = $test[2];
+                        $resourceID[] =  $test[3];
+                    }
+                }
+                echo '</table>';
+                if ($countNonCompliant == 0) {
+                    echo '<p>There is no resourses which do not comply with this rule</p>';
+                }
+                echo '</div><br>';
+                $i = 0;
+                echo '<br><h1>COMPLIANT RESOURCES</h1>';
+                echo '<div class="card">';
+
+                $countCompliant = 0;
+                $query = ('CALL get_resource_for_rules(:rID, :cID)');
+                $stmt = $mysql->prepare($query);
+                $stmt->bindValue(':rID', $_SESSION['rule']);
+                $stmt->bindValue(':cID', 1);
+                $stmt->execute();
+                $noncompliant = $stmt->fetchAll();
+                foreach ($myArr as $item) {
+                    if ($countCompliant == 0) {
+                        echo '
+                <table class="table">
+                <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Resource Name</th>
+                    <th scope="col">Exempt</th>
+                    <th scope="col">Review Date</th>
+                    <th scope="col">Last Updated</th>
+                    <th scope="col">Exceptions</th>
+                </tr>
+                </thead>';
+                    }
+                    echo '<tr>';
+                    echo '<td>', $myArr[$countCompliant], '</td>';
+                    echo '<td>Yes</td>';
+                    echo '<td>', $reviewDate[$countCompliant], '</td>';
+                    echo '<td>', $lastUpdated[$countCompliant], '</td>';
+                    echo ' <td> <form action="" method="post"><button name="view" value=', $resourceID[$countCompliant], ' class="btn btn-info">View</button></td> </form>';
+                    echo '</tr></div>';
+                    $countCompliant += 1;
+                }
+
+                foreach ($noncompliant as $non) {
+                    foreach ($result as $row) {
+                        if ($row[0] == $non[0]) {
+                            $i += 1;
+                        }
+                    }
+                    if ($i == 0) {
+                        if ($countCompliant == 0) {
+                            echo '
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Resource Name</th>
+                    <th scope="col">Exempt</th>
+                    <th scope="col">Review Date</th>
+                    <th scope="col">Last Updated</th>
+                    <th scope="col">Exceptions</th>
+                </tr>
+            </thead>';
+                        }
+                        echo '<tr>';
+                        echo '<td>', $non[0], '</td>';
+                        echo '<td>N/A</td>';
+                        echo '<td>N/A</td>';
+                        echo '<td>N/A</td>';
+                        echo ' <td> <form action="" method="post"> <button name="view" value=', $non[1], ' class="btn btn-info">View</button></td> </form>';
+                        echo '</tr>';
+                        $countCompliant += 1;
+                    }
+                }
+                echo '</table>';
+                if ($countCompliant == 0) {
+
+                    echo '<p style="text-align:center">There is no resourses which do comply with this rule</p></div>';
+                }
+
+                ?>
+
+
+                </table>
+            </div>
+            </div>
+            </div>
+            <br>
+            <footer>
+                <p>Visit our website:<br>
+                    <a class="footer-link" href="https://www.brightsolid.com/">BrightSolid</a>
+                </p>
+            </footer>
 </body>
 
 </html>
