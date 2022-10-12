ecd3c5f8192a0df29f8073110de6b93da425a345
Idris Mahamdi <73561935+idrismahamdi@users.noreply.github.com>
2022-10-12 16:48:07 +0100

    Update create.php

--- create.php
+++ create.php
@@ -1,6 +1,14 @@
 <?php
 require "connection.php";
 session_start();
+
+if ($_SESSION['user_role'] == 2) {
+    echo '<script type="text/javascript">';
+    echo 'alert("You do not have permission to create an exception");';
+    echo 'window.location.href = "view.php";';
+    echo '</script>';
+}
+
 if ($_SESSION['loggedin'] == false) {
     header('Location: login.php');
     die();
@@ -11,25 +19,45 @@ if (isset($_POST['logout'])) {
     die();
 }
 
-if (isset($_POST['suspend'])) {
-    $query = ('CALL suspend_exception(:rID,1)');
+if (isset($_POST['submit'])) {
+    $date = new DateTime('now');
+    if ($_POST['reviewDate'] == "oneMonth") {
+        $date->modify('+1 month'); // or you can use '-90 day' for deduct
+    } else if ($_POST['reviewDate'] == "threeMonth") {
 
+        $date->modify('+3 month');
+    } else  if ($_POST['reviewDate'] == "sixMonth") {
+        $date->modify('+6 month');
+    } else if ($_POST['reviewDate'] == "oneYear") {
+        $date->modify('+12 month');
+    }
+    $date = $date->format('Y-m-d h:i:s');
+    echo $date;
+    $current = new DateTime('now');
+    $current = $current->format('Y-m-d h:i:s');
+
+    $query = ("INSERT INTO exception(exception_id,customer_id,rule_id,resource_id,last_updated_by,exception_value,justification,review_date,last_updated) VALUES (NULL,:cID,:ruleID,:resourceID,:lastupdatedby,'bsol-dev-bakery-assets',:justify,:reviewDate,:lastUpdated);");
     $stmt = $mysql->prepare($query);
-    $stmt->bindValue(':rID', $_POST['suspend']);
+    $stmt->bindValue(':cID', $_SESSION['cID']);
+    $stmt->bindValue(':resourceID', $_SESSION['create']);
+    $stmt->bindValue(':ruleID', $_SESSION['rule']);
+    $stmt->bindValue(':justify', $_POST['justifcation']);
+    $stmt->bindValue(':reviewDate', $date);
+    $stmt->bindValue(':lastUpdated', $current);
+    $stmt->bindValue(':lastupdatedby', $_SESSION['user_id']);
     $stmt->execute();
+    echo '<script type="text/javascript">';
+    echo 'alert("Exception Added");';
+    echo 'window.location.href = "dashboard.php";';
+    echo '</script>';
 }
 
-if (isset($_POST['update'])) {
-    $_SESSION['update'] = $_POST['update'];
-    header('Location: update.php');
-    die();
-}
+
 ?>
 <!doctype html>
 <html>
 
 <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -40,7 +68,7 @@ if (isset($_POST['update'])) {
 
 <body>
     <header>
-        <nav class="navbar">
+        <nav class="navbar border">
             <div class="container">
                 <a class="navbar-brand" href="#">
                     <img src="logo.png" height="50px" alt="brightsolid">
@@ -51,190 +79,75 @@ if (isset($_POST['update'])) {
             </div>
         </nav>
     </header>
-
     <br>
     <nav aria-label="breadcrumb" class="main">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="dashboard.php">Summary Dashboard</a></li>
             <li class="breadcrumb-item"><a href="report.php">Detailed Report</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Exception History</li>
+            <li class="breadcrumb-item active" aria-current="page">Exception Form</li>
         </ol>
     </nav>
-
     <br>
-    <!--insert name of resource here-->
-    <h1>EXCEPTIONS</h1>
-    <?php
-    $query = ('SELECT resource_name FROM resource WHERE resource_id = :rID');
-
-    $stmt = $mysql->prepare($query);
-    $stmt->bindValue(':rID', $_SESSION['view']);
-
-    $stmt->execute();
-    $result = $stmt->fetch();
-
-    echo ' <h2>of Resource', $result, '</h2>'; ?>
-
-
-
+    <h1>CREATE AN EXCEPTION</h1>
     <br>
     <div class="main">
         <div class="card">
-            <table class="container text-center overflow-scroll">
-                <?php
+            <form class="exception-form" form action="" method="post">
+                <fieldset class='row mb-3 align-items-center'>
+                    <legend class='col-2 col-form-label'><strong><u>Review in:</u></strong></legend>
+                    <div class="col col-auto">
+                        <div class='btn-group' role='group' aria-label='Review date radio toggle button group'>
+                            <input type='radio' class='btn-check' name='reviewDate' id='oneMonth' value='oneMonth'
+                                autocomplete='off'>
+                            <label class='btn btn-outline-primary m-0' for='oneMonth'>1 Month</label>
+                            <input type='radio' class='btn-check' name='reviewDate' id='threeMonths' value='threeMonths'
+                                autocomplete='off'>
+                            <label class='btn btn-outline-primary m-0' for='threeMonths'>3 Months</label>
 
-                $query = ('CALL get_exceptions(:rID,:cID)');
-                if (!empty($_SESSION['view'])) {
+                            <input type='radio' class='btn-check' name='reviewDate' id='sixMonths' value='sixMonths'
+                                autocomplete='off'>
+                            <label class='btn btn-outline-primary m-0' for='sixMonths'>6 Months</label>
 
-
-                    $stmt = $mysql->prepare($query);
-                    $stmt->bindValue(':rID', $_SESSION['view']);
-                    $stmt->bindValue(':cID', $_SESSION['cID']);
-
-                    $stmt->execute();
-                    $result = $stmt->fetchAll();
-                }
-
-                $count = 0;
-                foreach ($result as $row) {
-
-                    if ($count == 0) {
-
-                        echo '    <h1>CURRENT EXCEPTIONS</h1>
-                        <tr>
-            <th>Rule</th>
-            <th>Last Updated By</th>
-            <th>Justification</th>
-            <th>Review Date</th>
-            <th>Last Updated</th>
-        </tr>';
-                    }
-
-                    echo '
-        <tr>
-            <td>
-                ', $row[0], '
-            </td>
-            <td>
-            ', $row[1], '
-            </td>
-            <td>
-            ', $row[2], '
-            </td>
-            <td>
-            ', $row[3], '
-            </td>
-            <td>
-            ', $row[4], '
-            </td>
-            <td>';
-                    if ($_SESSION['user_role'] == 1) {
-
-                        echo ' <form action="" method="post">
-                <button name="update" value=', $row[5], ' class="btn btn-info">Update</button> 
-                <form action="" method="post">
-            </td>
-            <td>
-            <form action="" method="post">
-                <button name="suspend" value=', $row[5], ' class="btn btn-info">Suspend</button>  
-                <form action="" method="post">
-            </td>
-   
-        </tr> ';
-                    }
-                    $count += 1;
-                }
-
-                if ($count == 0) {
-                    echo '<p style="text-align:center">There is no current exceptions for this resource.</p>';
-                }
-
-                ?>
-            </table>
+                            <input type='radio' class="btn-check" name='reviewDate' id='oneYear' value='oneYear'
+                                autocomplete='off'>
+                            <label class='btn btn-outline-primary m-0' for='oneYear'>1 Year</label>
 
 
+                        </div>
+                    </div>
 
+                </fieldset>
+
+                <div class='row mb-3 justify-content-start'>
+                    <div class='col-2'>
+                        <label for='justification' class='form-label'><strong><u>Justification:</strong></u></label>
+                    </div>
+                    <div class='col'>
+                        <textarea class='form-control' name='justifcation' id='justification' rows='4'
+                            required></textarea>
+                    </div>
+                </div>
+
+                <div class=" row mb-3 justify-content-start">
+                    <div class="col offset-2">
+                        <button class="btn btn-primary col" name="submit" type="submit">Create
+                            exception</button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
-
-
-    <div class="main">
-        <div class="card">
-            <table class="container text-center overflow-scroll">
-                <?php
-
-                $query = ('CALL get_Exception_History_For_Resource(:cID,:rID)');
-                if (!empty($_SESSION['view'])) {
-                    $stmt = $mysql->prepare($query);
-                    $stmt->bindValue(':rID', $_SESSION['view']);
-                    $stmt->bindValue(':cID', $_SESSION['cID']);
-                    $stmt->execute();
-                    $result = $stmt->fetchAll();
-                }
-
-                $count = 0;
-                foreach ($result as $row) {
-
-                    if ($count == 0) {
-                        if (($row[3] == NULL) || ($row[6] == NULL)) {
-                            $row[3] = 'N/A';
-                            $row[5] = 'N/A';
-                        }
-                        echo '    <h1>HISTORIC EXCEPTIONS</h1>
-                        <tr>
-                    
-            <th>Action</th>
-            <th>Date of Action</th>
-            <th>Exception Value</th>
-            <th>New Justifcation</th>
-            <th>Old Justifcation</th>
-            <th>New Review Date</th>
-            <th>Old Review Date</th>
-            
-        </tr>';
-                    }
-
-                    echo '
-        <tr>
-            <td>
-                ', $row[0], '
-            </td>
-            <td>
-            ', $row[1], '
-            </td>
-            <td>
-            ', $row[2], '
-            </td>
-            <td>
-            ', $row[3], '
-            </td>
-            <td>
-            ', $row[4], '
-            </td>
-            <td>
-            ', $row[5], '
-            </td>
-            <td>
-            ', $row[6], '
-            </td>
-   
-        </tr>
-        ';
-                    $count += 1;
-                }
-
-                if ($count == 0) {
-                    echo '<p style="text-align:center">There is no exception history for this resource.</p>';
-                }
-
-                ?>
-            </table>
-
-
-
-        </div>
-    </div>
-    </div>
+    <script async src='https://cdn.jsdelivr.net/npm/es-module-shims@1/dist/es-module-shims.min.js'
+        crossorigin='anonymous'></script>
+    <script type='importmap'>
+        {
+    "imports": {
+    "@popperjs/core": "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js",
+      "bootstrap": "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.esm.min.js"
+    }
+  }
+</script>
+    <script type="module" src="Components/ExceptionForm/exception-form.js"></script>
     <br>
     <footer>
         <p>Visit our website:<br>
