17a204881a3ba4454b87451e4663cabd45b01841
Idris Mahamdi <73561935+idrismahamdi@users.noreply.github.com>
2022-10-14 21:25:01 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -10,4 +10,19 @@ $mysql = new PDO(
     $username,
     $password
 );
+$date = new DateTime('now');
+$date = $date->format('Y-m-d h:i:s');
 
+$query = ('SELECT exception_id, review_date FROM exception');
+$stmt = $mysql->prepare($query);
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+foreach ($result as $row) {
+    if ($date > $row[1]) {
+        $query = ('CALL expired_exception(:rID)');
+        $stmt = $mysql->prepare($query);
+        $stmt->bindValue(':rID', $row[0]);
+        $stmt->execute();
+    }
+}
